f7522762333eb318b852f1759398bb452ce944b1
cesar <cesolis1990@gmail.com>
2020-08-05 23:15:14 -0500

    removing commented code

--- src/com/uag/cesar/solis/Main.java
+++ src/com/uag/cesar/solis/Main.java
@@ -1,26 +1,8 @@
 package com.uag.cesar.solis;
 
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
-      /*final Board board = new Board();
-      System.out.println("Starting board...");
-      BoardUtils.printBoardState(board.getBoardState());
-      //board.moveDisc(Disc.WHITE,
-      //    BoardRow.getBoardRowForStringValue("3"),
-      //    BoardColumn.getBoardColumnForStringValue("E"));
-      //BoardUtils.printBoardState(board.getBoardState());
-      final List<int[][]> possibleMoves = BoardUtils.getPossibleMoves(
-          Disc.BLACK,
-          board.getBoardState()
-      );
-
-      for (int[][] move : possibleMoves) {
-        System.out.println("Possible move...");
-        BoardUtils.printBoardState(move);
-      }*/
       final Game game = new Game(false);
       game.start();
     }
